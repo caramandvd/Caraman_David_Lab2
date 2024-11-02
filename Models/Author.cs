8ea90d09688090632489f9acbc71f2a9f3653f73
caramandvd <117540870+caramandvd@users.noreply.github.com>
2024-11-02 22:51:34 +0200

    Select Publishers- date relationale

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,10 +1,20 @@
-﻿namespace Caraman_David_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Caraman_David_Lab2.Models
 {
     public class Author
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
         public ICollection<Book>? Books { get; set; }
     }
 }
